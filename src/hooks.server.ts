b807c7110c4e1c27ee5f0d8b84fc0ec1eccedcc3
mabby1110 <rznns.mabby@gmail.com>
2024-07-22 17:34:48 -0600

    fix: hooks.server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,14 +7,7 @@ import { pb } from "$lib/pocketbase";
 import { fail } from "@sveltejs/kit";
 import PocketBase from "pocketbase";
 
-try {
-    const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL;
-    const POCKETBASE_PRIVATE_EMAIL = import.meta.env.POCKETBASE_PRIVATE_EMAIL;
-    const POCKETBASE_PRIVATE_PASSWORD = import.meta.env.POCKETBASE_PRIVATE_PASSWORD;
-    const apiKey = import.meta.env.VITE_API_KEY;
-} catch{}
-
-export const handle = async ({event, resolve}) => {
+export const handle = async ({ event, resolve }) => {
     event.locals.pocketBase = new PocketBase(POCKETBASE_URI);
 
     try {
@@ -32,24 +25,25 @@ export const handle = async ({event, resolve}) => {
     try {
         await event.locals.pocketBaseAdmin.admins.authWithPassword(
             POCKETBASE_PRIVATE_EMAIL,
-            POCKETBASE_PRIVATE_PASSWORD,
+            POCKETBASE_PRIVATE_PASSWORD
         );
     } catch (error) {
-        throw fail(500);
+        console.error("Error authenticating with PocketBase admin:", error);
+        return fail(500, { message: "Internal Server Error" });
     }
 
     pb.set(event.locals.pocketBase);
 
-    const response = await resolve(event)
+    const response = await resolve(event);
 
     response.headers.set(
         "set-cookie",
         event.locals.pocketBase.authStore.exportToCookie({
             sameSite: true,
-            secure: false,
-            httpOnly: false,
+            secure: true,
+            httpOnly: true,
         })
     );
 
     return response;
-}
+};
