4cfd082307df7337c02032a55303a93bd76cb647
mabby1110 <rznns.mabby@gmail.com>
2024-08-05 20:15:20 -0600

    Segunda seccion landing

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,7 +2,7 @@
     import AnimatedElement from "$lib/components/AnimatedElement.svelte";
     import Link from "$lib/components/Link.svelte";
     import mapa from "$lib/images/mapa.svg"
-    import logo from "$lib/images/C de comercios LOGO.svg"
+    import logo from "$lib/images/C DE COMERCIO LOGO.svg"
     import { onMount } from 'svelte';
     import { release } from '$lib/bgAnimation';
 
@@ -26,14 +26,14 @@
 <div class="home">
 <AnimatedElement>
     <section class="heroSection">
-        <div class="heroTitle">
+        <div class="title">
             <div class="que"><p>¿Que</p></div>
             <div class="se"><p>SE</p></div>
             <div class="vende"><p>VENDE</p></div>
             <div class="en"><p>en ...</p></div>
             <div class="abeja"><iconify-icon icon="fluent-emoji-high-contrast:honeybee" width="16vw" height="16vw"></iconify-icon></div>
         </div>
-        <div class="heroVisuals">
+        <div class="visuals">
             <h1>Guadalajara</h1>
             <img src={mapa} alt="mapa de la ciudad">
             <div class="heroDescription">
@@ -44,8 +44,14 @@
 </AnimatedElement>
 
 <AnimatedElement>
-    <section>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod consectetur, deserunt, cum in nostrum cupiditate mollitia praesentium eius magnam esse quam nobis voluptas aspernatur voluptates alias enim. Adipisci necessitatibus, sit quae aliquid aliquam, fuga, iure quo provident ratione veritatis ea reprehenderit ipsam eius? Harum nemo molestiae recusandae ea. Et, ad.
+    <section class="misionSection">
+        <div class="title">
+            <h3>Nuestra misión es digitalizar, centralizar y exponer todos los negocios locales, permitiendo que cualquier persona disponga de un sitio web y un espacio para desarrollar su trabajo, ya sea vender un producto u ofrecer algún servicio.</h3>
+        </div>
+        <div class="visuals">
+            <h1>Misión</h1>
+            <img src={logo} alt="logo">
+        </div>
     </section>
 </AnimatedElement>
 
@@ -81,12 +87,12 @@ section {
     column-gap: 4rem;
     grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
 }
-.heroTitle {
+.title {
     display: grid;
     grid-template-rows: repeat(12, minmax(1rem, auto));
     grid-template-columns: repeat(12, minmax(1rem, auto));
 }
-.heroVisuals {
+.visuals {
     display: grid;
     grid-template-rows: repeat(12, minmax(1rem, auto));
     grid-template-columns: repeat(12, minmax(1rem, auto));
@@ -145,4 +151,34 @@ section {
     border-radius: 10px;
     box-shadow: 5px 5px 20px rgba(255, 255, 255, 0.281);
 }
+
+.misionSection {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+}
+
+.misionSection h1 {
+    grid-area: 1 / 1 / 7 / -1;
+    justify-self: center;
+    align-self:self-end;
+    font-size: 450%;
+}
+.misionSection img {
+    grid-area: 4 / 1 / -1 / -1;
+    justify-self: center;
+    align-self: center;
+    width: 20rem;
+}
+.misionSection h3 {
+    grid-area: 1 / 1 / -1 / -1;
+    align-self: center;
+    justify-self: center;
+    line-height: 1.8rem;
+    font-size: x-large;
+    background-color: var(--primary-color);
+    border-radius: 2rem;
+    padding: 2rem;
+    filter: drop-shadow(10px 10px 5px black);
+}
+
 </style>
